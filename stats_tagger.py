cb63b4abfac2fc21185763d2120818dbf38b0f27
Jake Murray-McGuirk <X00197631@mytudublin.ie>
2025-11-17 12:41:42 +0000

    Testing inputs

--- stats_tagger.py
+++ stats_tagger.py
@@ -96,7 +96,7 @@ def parse_event(event):
     return [event_name, outcome_name, player_no]
 
 # Test
-test_inputs = ["s22", "sw15", "tisu4", "ko22won", "ss23", "sw15", "se", "ss", "kos", "kow", "kowon", "kolost", "kol", "kog"]
+test_inputs = ["s22", "sw15", "tisu4", "ko22won", "ss23", "sw15", "se", "ss", "kos", "kow", "kowon", "kolost", "kol", "kog", "f16", "fg15", "f"]
 
 for code in test_inputs:
     print(code, "->", parse_event(code))

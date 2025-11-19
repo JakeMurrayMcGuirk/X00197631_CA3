492016c560e1adbad235acbe7e2d03d60ed7c348
Jake Murray-McGuirk <X00197631@mytudublin.ie>
2025-11-19 18:21:15 +0000

    Revert "Merged development into master"
    This reverts commit de69af72f0e771f3bd27002771699766b199cbf1, reversing
    changes made to cba0aa06f222fbccecd5318b5dc75405f22d38e6.

--- stats_tagger.py
+++ stats_tagger.py
@@ -1,56 +1,23 @@
-import re
-from utils import events, event_shortcuts, outcomes, outcome_shortcuts
+team1 = input("Enter team 1 name: ")
+team2 = input("Enter team 2 name: ")
 
+events = [
+    'start game',
+    'end half',
+    'start half',
+    'end game',
+    'shot',
+    'kickout',
+    'tackle',
+    'pass',
+    'foul'
+]
 
-def get_event(event):
-    # Used ChatGPT to help with function to match event shortcuts WITHIN text
-    for e in event_shortcuts:
-        if event.startswith(e):
-            # Return the shortcut code, full event name, and remaining text
-            return e, event_shortcuts[e], event[len(e):]
-    return None, None, event
+outcomes = {
+    'shot' : ['scored', 'wide'],
+    'kickout' : ['won', 'lost'],
+    'tackle' : ['successful', 'incomplete'],
+    'pass' : ['complete', 'incomplete']
+}
 
-def get_outcome(event, remaining_text):
-    # Checks if the event is in a list of possible outcomes
-    if event not in outcomes:
-        return None, remaining_text
-    
-    # Get possible list of outcome shortcuts from dictionary
-    possible_outcomes = outcomes[event]
-    
-    for o in sorted(possible_outcomes, key = len, reverse = True):
-        # If text starts with outcome shortcut
-        if remaining_text.startswith(o):
-            # Returns outcome shortcut, remaining text
-            return outcome_shortcuts[o], remaining_text[len(o):]
-    return None, remaining_text
-
-
-def get_player_no(remaining_text):
-    player_no = re.search("\d+$", remaining_text)
-    if player_no:
-        player_no = player_no.group()
-        return player_no
-    return None
-
-
-def parse_event(event):
-    # Get event code and format it to remove all whitespace and make lowercase
-    event = event.strip().lower().replace(" ","")
-    # Used Chat GPT to help generate the below 3 lines
-    event_code, event_name, remaining = get_event(event)
-    if event_name is None:
-        return None
-    
-    # Get outcome code
-    outcome_name, remaining = get_outcome(event_name, remaining)
-
-    # Get player no
-    player_no = get_player_no(remaining)
-
-    # Return event, outcome and player number
-    return [event_name, outcome_name, player_no]
-
-def inputEvent():
-    e = input("Enter match event: ")
-    return e
+match_events=[]

0a36a5d874dfff849c5c1257c8bcc7760172a2d8
Jake Murray-McGuirk <X00197631@mytudublin.ie>
2025-11-19 17:55:24 +0000

    Fixed formatting on event.strip() in lines 9 and 10

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ match_events = []
 print("Enter an event in the format 'event outcome(if applicable) player no. (if applicable)\nIf outcome or player no. not available, skip that section.\nE.g. foul commited by player 14: f14\nShot wide from player 12: sw14\nStart game:sg")
 event = inputEvent()
 
-while event.strip.lower()!="end game":
-    event = event.strip.lower()
+while event.strip().lower()!="end game":
+    event = event.strip().lower()
     if event in commands:
         print(commands[event])
     else:
